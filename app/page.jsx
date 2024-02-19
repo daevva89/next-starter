480afecbde73fcf69aa51c3b3d6958b4f39ca076
Mihu <mihai.ilie@rebag.com>
2024-02-19 23:14:43 +0200

    Initial commit

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <section className='py-24'>
+      <div className='container'>
+        <h1 className='mt-4 text-center text-3xl font-bold'>
+          Next JS 13 Starter Kit
+        </h1>
+      </div>
+    </section>
+  )
+}
